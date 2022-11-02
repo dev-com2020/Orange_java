73c0646829cef493e29b8262909134dac0a2e668
devcom2020 <kwp@tlen.pl>
2022-11-02 16:06:03 +0100

    sroda 16:00

---
+++ src/main/java/Zad1.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class Zad1 {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Podaj pierwszą liczbę");
+//        double numberA = sc.nextDouble();
+//        System.out.println("Podaj drugą liczbę");
+//        double numberB = sc.nextDouble();
+//        System.out.println("*******************");
+//        System.out.printf("%, .3f\n", (numberA / numberB));
+//        System.out.println("*******************");
+        System.out.printf("%tF\n", new Date());
+        System.out.printf("%tT\n", new Date());
+        System.out.printf("%tb\n", new Date());
+        System.out.printf("%tA\n", new Date());
+        System.out.printf("%te\n", new Date());
+
+
+    }
+}
