78ff892c738215fb3a5d5b4b49aaf550ba7ac0d6
devcom2020 <kwp@tlen.pl>
2022-11-03 15:16:28 +0100

    czwartek 15:16

--- src/main/java/data/Car.java
+++ src/main/java/data/Car.java
@@ -1,21 +1,34 @@
 package data;
 
-class Car {
-    int year;
-    String brand;
-    String model;
-    String color;
+public class Car {
+    private int year;
+    private String brand;
+    private String model;
+    private String color;
+
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
 
     // konstruktor1
-    Car(int year, String brand, String model, String color) {
+    public Car(int year, String brand, String model, String color) {
         this(year, brand, model);
         this.color = color;
     }
 
     // konstruktor2
-    Car(int year, String brand, String model) {
+    public Car(int year, String brand, String model) {
         this.year = year;
         this.brand = brand;
         this.model = model;
+    }
+    public void print(){
+        System.out.println(brand + " " + model + " " + year + " " + color);
     }
 }
---
+++ src/main/java/logic/CarShop.java
@@ -0,0 +1,17 @@
+package logic;
+
+import data.Car;
+
+class CarShop {
+    public static void main(String[] args) {
+        Car audiA5 = new Car(2014, "Audi", "A5", "Czarne");
+        Car audiA4 = new Car(2015, "Audi", "A4");
+        //w międzyczasie klient wybiera kolor:
+        audiA4.setColor("Czerwony");
+
+        audiA5.print();
+        audiA4.print();
+        String audiA4color = audiA4.getColor();
+        System.out.println(audiA4color);
+    }
+}
