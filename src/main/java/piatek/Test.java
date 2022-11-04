0c645c8e32e0eaf54d217080b24f0aff3cd19308
devcom2020 <kwp@tlen.pl>
2022-11-04 10:01:28 +0100

    piatek 10:01

--- src/main/java/piatek/Test.java
+++ src/main/java/piatek/Test.java
@@ -10,21 +10,26 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Podaj liczbę:");
-        int number = 0;
+        int[] numbers = new int[2];
         boolean error = true;
         do {
             try {
-                number = sc.nextInt();
+                System.out.println("Podaj 1 liczbę:");
+                numbers[0] = sc.nextInt();
+                System.out.println("Podaj 2 liczbę:");
+                numbers[1] = sc.nextInt();
+                System.out.println("Którą wartość wyświetlić(1 lub 2)");
+                System.out.println("Liczba: " + numbers[sc.nextInt() - 1]);
                 error = false;
-                System.out.println("Wczytano poprawną liczbę");
             } catch (InputMismatchException e) {
                 System.err.println("Podana wartość jest nieprawidłowa! Spróbuj jeszcze raz...");
                 sc.nextLine();
 //            e.printStackTrace();
+            } catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Miało być 1 albo 2!!!,zacznijmy od nowa!");
+                sc.nextLine();
             }
         } while (error);
-        System.out.println("Podałeś: " + number);
         sc.close();
     }
 }
